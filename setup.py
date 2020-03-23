659a77327fc218de0486fe4d78bb99047c34e443
Ella Wang <xi.wang3@anu.edu.au>
2020-03-23 18:28:36 +1100

    Added dependency

--- setup.py
+++ setup.py
@@ -137,7 +137,8 @@ setup(
                       'numpy>=1.15',
                       'scikit-learn>=0.21',
                       'scipy>=1.3',
-                      'pytest>=3.5'],  # Optional
+                      'pytest>=3.5',
+                      'pykrige>=1.4'],  # Optional
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). Users will be able to install these using the "extras"
