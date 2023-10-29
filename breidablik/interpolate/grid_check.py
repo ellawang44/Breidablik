5bccfee5b002ced7de4c3d82d5f279903e517b80
Ella Wang <ella.xi.wang@gmail.com>
2023-10-29 15:39:43 +1100

    regularise grid

--- breidablik/interpolate/grid_check.py
+++ breidablik/interpolate/grid_check.py
@@ -11,11 +11,12 @@ def _grid_check(eff_t, surf_g, met):
     with open(_base_path.parent / 'grid_snapshot.txt', 'r') as f:
         t_step, m_step = np.float_(f.readline().split())
         grid = np.loadtxt(f)
+    # round the teffs
+    grid[:,0] = np.round(grid[:,0]*2)/2
     input_model = np.array([eff_t*t_step, surf_g, met*m_step])
     input_tile = np.tile(input_model, (grid.shape[0], 1))
     min_dist = min(np.sqrt(np.sum(np.square(grid - input_tile), axis = 1)))
-    # the maximum minimum distance between points in the grid is 0.5063
-    # np.sqrt(3*0.25**2) = 0.4330
+    #TODO: squares? circles/squares = 2.72
     # there should be no holes in the grid
     if min_dist > np.sqrt(3*0.25**2):
         warnings.warn('Input stellar parameters are outside of the grid, results are extrapolated and may not be reliable. Stellar parameters inputted were: teff = {0:.2f}, surfg = {1:.2f}, met = {2:.2f}'.format(eff_t, surf_g, met))
